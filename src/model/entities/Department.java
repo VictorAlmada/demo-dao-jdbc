83c337c77f49c9c364f179fdfc26342717f63c81
Victor Almada <victor_mathers@hotmail.com>
2024-09-20 17:58:19 -0300

    Department entity class

---
+++ src/model/entities/Department.java
@@ -0,0 +1,23 @@
+package model.entities;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@EqualsAndHashCode(of = "id")
+@ToString
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
+public class Department implements Serializable {
+	private static final long serialVersionUID = 1L;
+	// attributes
+	private Integer id;
+	private String name;
+
+	
+}
